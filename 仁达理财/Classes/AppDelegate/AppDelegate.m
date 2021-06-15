bc7c7c10a3aacf69cf50f4255f2bc615c9750cf9
Cio8a3NS <ougj41cypn31818@126.com>
2021-06-15 17:19:36 +0800

    test

--- 仁达理财/Classes/AppDelegate/AppDelegate.m
+++ 仁达理财/Classes/AppDelegate/AppDelegate.m
@@ -18,6 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     self.window = [[UIWindow alloc]init];
     
     self.window.frame = [UIScreen mainScreen].bounds;
